7e6d3864e7b7c2d5e8a4346ea913f5c668cc71b1
Julian Sandoval <sandoval@middlebury.edu>
2023-10-30 12:21:32 -0400

    dorm-page view

---
+++ src/components/dormMaps.js
@@ -0,0 +1,5 @@
+// import React, { useState } from "react";
+// import PropTypes from "prop-types";
+// import Image from "next/image";
+// import campusmap from "../images/campusmap_720.png";
+// import styles from "../styles/InteractiveMap.module.css";
