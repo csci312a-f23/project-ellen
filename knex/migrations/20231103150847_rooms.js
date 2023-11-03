407262110e48ddbe5fec79102ec0395e3607edbd
Siri <seahern@middlebury.edu>
2023-11-03 11:55:21 -0400

    adding knex files/db

---
+++ knex/migrations/20231103150847_rooms.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// eslint-disable-next-line func-names
+exports.up = function (knex) {
+  return knex.schema.createTable("Room", (table) => {
+    table.increments("id").primary();
+    table.string("dormName");
+    table.string("dormDimensions");
+    table.text("dormReview");
+    table.string("dormRating");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// eslint-disable-next-line func-names
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("Room");
+};
