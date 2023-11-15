fcfd4b40d9b0aeb425da62c4b0ae04025640bed4
EpicET <etowner@middlebury.edu>
2023-11-15 12:02:49 -0500

    h

--- src/pages/api/rooms/[id].js
+++ src/pages/api/rooms/[id].js
@@ -10,7 +10,6 @@ router
   })
 
   .put(async (req, res) => {
-    // eslint-disable-next-line no-unused-vars
     const { id, ...updatedRoom } = req.body;
     // req.query.id is a string, and so needs to be converted to an integer before comparison
     if (id !== parseInt(req.query.id, 10)) {
@@ -20,31 +19,9 @@ router
     }
 
     const room = await Room.query()
-      .findById(id)
-      .throwIfNotFound()
-      .withGraphFetched("reviews");
-
+      .updateAndFetchById(req.query.id, updatedRoom)
+      .throwIfNotFound();
     res.status(200).json(room);
   });
 
 export default router.handler();
-// router
-//   .get(async (req, res) => {
-//     const room = await Room.query().findById(req.query.id).throwIfNotFound();
-//     console.log(room);
-//     res.status(200).json(room);
-//   })
-//   .put(async (req, res) => {
-//     const { id, ...updatedRoom } = req.body;
-//     // req.query.id is a string, and so needs to be converted to an integer before comparison
-//     if (id !== parseInt(req.query.id, 10)) {
-//       // Verify id in the url, e.g, /api/rooms/10, matches the id the request body
-//       res.status(400).end(`URL and object does not match`);
-//       return;
-//     }
-
-//     const room = await Room.query()
-//       .updateAndFetchById(req.query.id, updatedRoom)
-//       .throwIfNotFound();
-//     res.status(200).json(room);
-//   });
