8b0869e9e15c8779b4d182d4e5e5ac52a9534a4f
EpicET <etowner@middlebury.edu>
2023-11-26 15:36:34 -0500

    remove auth part 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,20 +2,17 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 /* eslint-disable react/jsx-props-no-spreading,react/prop-types */
-import { SessionProvider } from "next-auth/react";
 
-export default function App({ session, Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
-      <div>
-        <Head className="justify-center text-center">
-          <title className="justify-center text-center">MiddHousing</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </SessionProvider>
+    <div>
+      <Head className="justify-center text-center">
+        <title className="justify-center text-center">MiddHousing</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
   );
 }
