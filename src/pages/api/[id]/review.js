34f8d57e3c792580833efec8cdd3535f2ff99adb
EpicET <etowner@middlebury.edu>
2023-11-26 14:26:24 -0500

    another

--- src/pages/api/[id]/review.js
+++
@@ -1,9 +0,0 @@
-// // import { createRouter } from "next-connect";
-// // import Review from "../../../../models/Review";
-
-// const router = createRouter();
-
-// router.post(async (req, res) => {
-//   const review = await Review.query().insert(req);
-//   res.status(200).json(review);
-// });
