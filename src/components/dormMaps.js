960392cab3cd28a774ef6ce6924a0b137ab414b7
chaggerty27 <144862861+chaggerty27@users.noreply.github.com>
2023-11-28 15:10:03 -0500

    testing things out

--- src/components/dormMaps.js
+++ src/components/dormMaps.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/DormMaps.module.css";
 
@@ -31,6 +31,41 @@ function DormMaps({ selectedDorm }) {
   };
   const selectedDormImages = dormImages[selectedDorm] || {};
 
+  const imageMapData = {
+    Battell: {
+      firstFloor: [
+        // Example coordinates for Room 101
+        { roomNumber: "101", coordinates: "0,0,150,150" },
+        // Add coordinates for other rooms on the first floor
+      ],
+      secondFloor: [
+        // Example coordinates for Room 201
+        { roomNumber: "201", coordinates: "100,100,200,200" },
+        // Add coordinates for other rooms on the second floor
+      ],
+    },
+    // Add entries for other dorms and their floors as needed
+  };
+
+  const selectedImageMapData = imageMapData[selectedDorm] || {};
+
+  const [tooltip, setTooltip] = useState("");
+
+  const handleMouseOver = (roomNumber) => {
+    setTooltip(`Room ${roomNumber}`);
+    console.log("hovering");
+  };
+
+  const handleMouseOut = () => {
+    setTooltip("");
+  };
+
+  const handleRoomClick = (roomNumber) => {
+    // Add navigation logic based on the clicked room number
+    // For example: router.push(`/dorms/${selectedDorm}/${roomNumber}`);
+    console.log(`Navigating to Room ${roomNumber}`);
+  };
+
   return (
     <div>
       <h1 className={styles.dormTitle}>{selectedDorm}</h1>
@@ -48,7 +83,29 @@ function DormMaps({ selectedDorm }) {
             width={700}
             src={imagePath}
             alt={`${floorName} image`}
+            useMap={`#${selectedDorm}-${floorName}-map`}
           />
+          <map
+            name={`${selectedDorm}-${floorName}-map`}
+            id={`${selectedDorm}-${floorName}-map`}
+          >
+            {selectedImageMapData[floorName]?.map(
+              ({ roomNumber, coordinates }) => (
+                <area
+                  key={roomNumber}
+                  shape="rect"
+                  coords={coordinates}
+                  alt={`Room ${roomNumber}`}
+                  data-room={roomNumber}
+                  onMouseOver={() => handleMouseOver(roomNumber)}
+                  onMouseOut={handleMouseOut}
+                  onClick={() => handleRoomClick(roomNumber)}
+                  className={styles.area}
+                />
+              ),
+            )}
+          </map>
+          {tooltip && <div className={styles.tooltip}>{tooltip}</div>}
         </div>
       ))}
     </div>
