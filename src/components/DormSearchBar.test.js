a5e1ef24682298c1957b5eb2a94bbcb1e1b7f5b4
EpicET <etowner@middlebury.edu>
2023-12-09 15:15:24 -0500

    Uncomented out test

--- src/components/DormSearchBar.test.js
+++ src/components/DormSearchBar.test.js
@@ -1,94 +1,32 @@
-// import React from "react";
-// import { render, screen, waitFor, fireEvent } from "@testing-library/react";
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import mockRouter from "next-router-mock";
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import userEvent from "@testing-library/user-event";
-// import "@testing-library/jest-dom";
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
-// import DormSearchBar from "./DormSearchBar";
+import React from "react";
+import mockRouter from "next-router-mock";
+import "@testing-library/jest-dom";
+import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
+import { render, screen } from "@testing-library/react";
 
-// jest.mock("next/router", () => ({
-//   useRouter: jest.fn(),
-// }));
+import DormSearchBar from "./DormSearchBar";
 
-// // Tell the mock router about the pages we will use (so we can use dynamic routes)
-// mockRouter.useParser(
-//   createDynamicRouteParser([
-//     // These paths should match those found in the `/pages` folder:
-//     // "/dorms/[...names]/[...room]", // something wrong with this one, I think it doesn't like the endpoint of [...room]
-//     "/dorms/[...name]",
-//     "/app",
-//     "/document",
-//     "/index",
-//     "/profile",
-//     "/review",
-//     "/rooms",
-//   ]),
-// );
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
 
-// describe("DormSearchBar", () => {
-//   beforeEach(() => {
-//     mockRouter.setCurrentUrl("/");
-//   });
-//   afterEach(() => {
-//     jest.resetAllMocks();
-//   });
+// Tell the mock router about the pages we will use (so we can use dynamic routes)
+mockRouter.useParser(
+  createDynamicRouteParser([
+    "/dorms/[...name]",
+    "/app",
+    "/document",
+    "/index",
+    "/profile",
+    "/review",
+    "/rooms",
+  ]),
+);
 
-//   test("Should display Battell rooms on initial render", () => {
-//     render(<DormSearchBar />);
-
-//     // just check for first few rooms
-//     const rooms = [
-//       "Room: 100",
-//       "Room: 101",
-//       "Room: 102",
-//       "Room: 103",
-//       "Room: 104",
-//       "Room: 105",
-//       "Room: 106",
-//       "Room: 107",
-//       "Room: 108",
-//     ];
-//     rooms.forEach((room) => {
-//       expect(screen.getByText(room)).toBeInTheDocument();
-//     });
-//   });
-
-//   test("Should filter rooms based on search term", async () => {
-//     render(<DormSearchBar />);
-
-//     const searchInput = screen.getByPlaceholderText("Search...");
-
-//     fireEvent.change(searchInput, { target: { value: "101" } });
-//     fireEvent.click(screen.getByRole("button", { name: "Search" }));
-
-//     await screen.findByRole("list", { name: "SearchBar-results" });
-
-//     const expectedResults = ["Room: 101"];
-//     const unexpectedResults = ["Room: 102", "Room: 103"]; // Add any other rooms
-
-//     expectedResults.forEach((room) => {
-//       expect(screen.getByText(room)).toBeInTheDocument();
-//     });
-//     unexpectedResults.forEach((room) => {
-//       expect(screen.queryByText(room)).toBeNull();
-//     });
-//   });
-
-//   test("Navigates to the correct page when a room is clicked", async () => {
-//     render(<DormSearchBar />);
-
-//     const roomNum = "Room: 100";
-//     const roomItem = screen.getByText(roomNum).closest("li");
-//     userEvent.click(roomItem);
-
-//     mockRouter.push("/100");
-
-//     await waitFor(() => {
-//       expect(mockRouter.pathname).toBe("/100");
-//       expect(mockRouter.asPath).toBe(`/${roomNum.split(" ").pop()}`);
-//     });
-//   });
-// });
+describe("DormSearchBar", () => {
+  test("Renders the search bar correctly", () => {
+    render(<DormSearchBar name="Battell" />);
+    const searchBar = screen.getByPlaceholderText("Search...");
+    expect(searchBar).toBeInTheDocument();
+  });
+});
