7379c8b00f72e3fef275680005d5dfa3155a1047
Siri <seahern@middlebury.edu>
2023-12-11 14:22:31 -0500

    adding rooms with authentication

--- src/pages/api/userProfile.js
+++ src/pages/api/userProfile.js
@@ -1,5 +1,6 @@
 // woooow
 import { createRouter } from "next-connect";
+import { authenticated } from "../../lib/middleware";
 import User from "../../../models/User";
 
 const router = createRouter();
@@ -14,24 +15,40 @@ router
     res.status(200).json(userProfile);
   })
 
-  .put(async (req, res) => {
-    const { id } = req.query;
+  .put(authenticated, async (req, res) => {
+    const { id, id2, roomData } = {
+      id: req.user.googleId,
+      id2: req.user.id,
+      ...req.body,
+    };
+    console.log(id);
+    console.log(id2);
+    console.log(roomData);
     if (!id) {
       res.status(401).json({ error: "Unauthorized" });
     }
-    const { roomData } = req.body;
 
-    const existingData = await User.query().findById(id);
+    const existingData = await User.query().findById(id2);
     res.status(200).json(existingData);
 
+    // If room1 is empty
     if (!existingData.room1) {
-      await User.query().updateAndFetchById(id, { room1: roomData });
+      await User.query().updateAndFetchById(id2, {
+        googleId: String(id),
+        room1: roomData,
+      });
     } else if (!existingData.room2) {
-      // Check if room2 is empty
-      await User.query().updateAndFetchById(id, { room2: roomData });
+      // Else if room2 is empty
+      await User.query().updateAndFetchById(id2, {
+        googleId: String(id),
+        room2: roomData,
+      });
     } else if (!existingData.room3) {
-      // Check if room3 is empty
-      await User.query().updateAndFetchById(id, { room3: roomData });
+      // Else if room3 is empty
+      await User.query().updateAndFetchById(id2, {
+        googleId: String(id),
+        room3: roomData,
+      });
     } else {
       res.status(400).end(`All rooms are occupied`);
     }
