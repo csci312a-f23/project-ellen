3df3d4ba79095a8bcffa7a8ba752f3293ec19b15
EpicET <etowner@middlebury.edu>
2023-12-13 00:07:03 -0500

    here some quick fixes

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
+
 const LeafletMap = () => {
   const mapContainerRef = useRef(null);
 
@@ -14,11 +15,13 @@ const LeafletMap = () => {
       17,
     );
 
+    // eslint-disable-next-line new-cap
     const defaultIcon = new L.icon({
-      iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg',
+      iconUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg",
       iconSize: [20, 30],
       iconAnchor: [10, 15],
-      popupAnchor: [0, 0]
+      popupAnchor: [0, 0],
     });
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -26,13 +29,21 @@ const LeafletMap = () => {
         '&copy; <a href="<https://www.openstreetmap.org/copyright>">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([44.011309, -73.178091], {icon: defaultIcon}).addTo(map).bindPopup('<a href="/dorms/Battell">Battell</a>');
+    L.marker([44.011309, -73.178091], { icon: defaultIcon })
+      .addTo(map)
+      .bindPopup('<a href="/dorms/Battell">Battell</a>');
 
-    L.marker([44.0129, -73.177270], {icon: defaultIcon}).addTo(map).bindPopup('<a href="/dorms/Allen">Allen</a>');
+    L.marker([44.0129, -73.17727], { icon: defaultIcon })
+      .addTo(map)
+      .bindPopup('<a href="/dorms/Allen">Allen</a>');
 
-    L.marker([44.008516 , -73.178805], {icon: defaultIcon}).addTo(map).bindPopup('<a href="/dorms/Hepburn">Hepburn</a>');
+    L.marker([44.008516, -73.178805], { icon: defaultIcon })
+      .addTo(map)
+      .bindPopup('<a href="/dorms/Hepburn">Hepburn</a>');
 
-    L.marker([44.007979 , -73.178643], {icon: defaultIcon}).addTo(map).bindPopup('<a href="/dorms/Stewart">Stewart</a>');
+    L.marker([44.007979, -73.178643], { icon: defaultIcon })
+      .addTo(map)
+      .bindPopup('<a href="/dorms/Stewart">Stewart</a>');
 
     return () => {
       map.remove();
