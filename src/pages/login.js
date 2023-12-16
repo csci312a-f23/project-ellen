a66ba4047c76e9a35434bfcd7d50b65c0f37db36
EpicET <etowner@middlebury.edu>
2023-12-16 17:39:49 -0500

    removed login header

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Header from "@/components/Header";
 import LoginWidget from "../components/LoginWidget";
 import styles from "../styles/login.module.css";
 
 export default function LoginPage() {
   return (
     <div className={styles.body}>
-      <Header home={false} profile={false} />
+      <div className={styles.h1}>
+        <img
+          className={styles.pantherImage}
+          height={100}
+          width={300}
+          src="/images/panther.png"
+          alt="panther"
+        />
+        <h3>Middlebury Housing</h3>
+      </div>
       <LoginWidget />
     </div>
   );
