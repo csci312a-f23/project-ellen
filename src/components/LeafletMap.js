0d11ac340f5106ff3e55e4f1515e132d243a876f
ndharris24 <104739309+ndharris24@users.noreply.github.com>
2023-12-17 17:35:39 -0500

    quick fixing eslint problem, whoops

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,21 +1,23 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable global-require */
+/* eslint-disable padded-blocks */
+/* eslint-disable spaced-comment */
 import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 
 const LeafletMap = () => {
   const mapContainerRef = useRef(null);
-  //define the leafletmap object
+  // define the leafletmap object
   useEffect(() => {
     //import leaflet as an object
     const L = require("leaflet");
-    //set the current view to Middlebury campus, with a zoom level of 17
+    // set the current view to Middlebury campus, with a zoom level of 17
     const map = L.map(mapContainerRef.current).setView(
       [44.011309, -73.178091],
       17,
     );
-    //This block replaces the missing picture image that happens with leaflet by creating a new custom icon using the wikimedia common pin icon SVG as its image.
+    // This block replaces the missing picture image that happens with leaflet by creating a new custom icon using the wikimedia common pin icon SVG as its image.
     const defaultIcon = new L.Icon({
       iconUrl:
         "https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg",
@@ -23,16 +25,16 @@ const LeafletMap = () => {
       iconAnchor: [10, 15],
       popupAnchor: [0, 0],
     });
-    //sets up the tiles to be displayed on the map (the actual image of the map) as well as giving credit to original package authors.
+    // sets up the tiles to be displayed on the map (the actual image of the map) as well as giving credit to original package authors.
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="<https://www.openstreetmap.org/copyright>">OpenStreetMap</a> contributors',
     }).addTo(map);
-    //adds Battell custom icon marker
+    // adds Battell custom icon marker
     L.marker([44.011309, -73.178091], { icon: defaultIcon })
       .addTo(map)
       .bindPopup('<a href="/dorms/Battell">Battell</a>');
-    //adds Allen custom icon marker
+    // adds Allen custom icon marker
     L.marker([44.0129, -73.17727], { icon: defaultIcon })
       .addTo(map)
       .bindPopup('<a href="/dorms/Allen">Allen</a>');
@@ -40,7 +42,7 @@ const LeafletMap = () => {
     L.marker([44.008516, -73.178805], { icon: defaultIcon })
       .addTo(map)
       .bindPopup('<a href="/dorms/Hepburn">Hepburn</a>');
-    //adds Stewart custom icon marker
+    // adds Stewart custom icon marker
     L.marker([44.007979, -73.178643], { icon: defaultIcon })
       .addTo(map)
       .bindPopup('<a href="/dorms/Stewart">Stewart</a>');
@@ -50,7 +52,7 @@ const LeafletMap = () => {
     };
   }, []);
 
-  //returns the CSS for the Leafletmap
+  // returns the CSS for the Leafletmap
   return (
     <div
       id="mapid"
@@ -60,7 +62,7 @@ const LeafletMap = () => {
   );
 };
 
-//this block makes sure that the leaflet map is only rendered on the client side, and not on the server side. Removing this WILL prevent the entire deployed version from running.
+// this block makes sure that the leaflet map is only rendered on the client side, and not on the server side. Removing this WILL prevent the entire deployed version from running.
 const DynamicLeafletMapWithNoSSR = dynamic(() => Promise.resolve(LeafletMap), {
   ssr: false,
 });
